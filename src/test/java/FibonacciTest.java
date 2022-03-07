ada7d8a75f87754c6645e07b2a0f12c03be8cd1f
cberdejo <cberdejo@uma.es>
2022-03-07 12:37:01 +0100

    Test hechos

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
@@ -14,29 +16,67 @@ import static org.junit.jupiter.api.Assertions.*;
  *fibonacci negative number
  */
 class FibonacciTest {
-
-    public void testFibonacci0(){
-
+    private Fibonacci fibonacci;
+    @BeforeEach
+    public void setup(){
+        fibonacci = new Fibonacci();
     }
+    @AfterEach
+    public void finish(){
+        fibonacci = null;
+    }
+    public void testFibonacci0(){
+        int expectedValue = 0;
+        int obtainedValue = fibonacci.compute(0);
+
+        assertEquals(expectedValue,obtainedValue); //JUnit
+    }
+
     @Test
     public void testFibonacci1(){
+        int expectedValue = 1;
+        int obtainedValue = fibonacci.compute(1);
 
+        assertEquals(expectedValue,obtainedValue); //JUnit
+    }
+    @Test
+    public void testFibonacci2(){
+        int expectedValue = 1;
+        int obtainedValue = fibonacci.compute(2);
+
+        assertEquals(expectedValue,obtainedValue); //JUnit
     }
     @Test
     public void testFibonacci3(){
+        int expectedValue = 2;
+        int obtainedValue = fibonacci.compute(3);
 
+        assertEquals(expectedValue,obtainedValue); //JUnit
     }
     @Test
     public void testFibonacci4(){
+        int expectedValue = 8;
+        int obtainedValue = fibonacci.compute(4);
 
+        assertEquals(expectedValue,obtainedValue); //JUnit
     }
     @Test
-    public void testFibonacci6(){
+    public void testFibonacci8(){
+        int expectedValue = 21;
+        int obtainedValue = fibonacci.compute(3);
+
+        assertEquals(expectedValue,obtainedValue); //JUnit
     }
     @Test
     public void testFibonacciNegativeNumber(){
+        Random rd = new Random();
 
+        int i = rd.nextInt();
+        while (i >= 0){
+            i = rd.nextInt();
+        }
 
-
+        int value = i;
+        assertThrows(RuntimeException.class, ()->fibonacci.compute(value)); //JUnit
     }
 }
