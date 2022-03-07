c57f83823158b4aa992012ddb442d0ff8d483dcb
cberdejo <cberdejo@uma.es>
2022-03-07 12:27:10 +0100

    Estructura hecha

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * class providing a method that computes the value of Fibonacci series of an Integer number
+ * @Author Christian Berdejo Sánchez
+ */
+
+public class Fibonacci {
+
+    public int  compute(int number){
+
+    }
+
+}
---
+++ src/test/java/FibonacciTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Test cases:
+ * fibonacci 0 -> 0
+ * fibonacci 1 -> 1
+ * fibonacci 2 -> 1
+ * fibonacci 3 -> 2
+ * fibonacci 4 ->3
+ * fibonacci 8 -> 21
+ *fibonacci negative number
+ */
+class FibonacciTest {
+
+    public void testFibonacci0(){
+
+    }
+    @Test
+    public void testFibonacci1(){
+
+    }
+    @Test
+    public void testFibonacci3(){
+
+    }
+    @Test
+    public void testFibonacci4(){
+
+    }
+    @Test
+    public void testFibonacci6(){
+    }
+    @Test
+    public void testFibonacciNegativeNumber(){
+
+
+
+    }
+}
